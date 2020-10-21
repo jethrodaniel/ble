7b33d025bce02de23c368ccfa026c3a9fa806197
Mark Delk <jethrodaniel@gmail.com>
2020-10-20 23:02:24 -0500

    more exploring

--- blutooth.py
+++ blutooth.py
@@ -4,11 +4,26 @@ from bluetooth.ble import DiscoveryService, GATTRequester
 
 # https://pypi.org/project/gattlib/
 
-print("Scanning for BLE devices...")
 
-# service = DiscoveryService("hci0")
-service = DiscoveryService()
-devices = service.discover(10)
+# Generic Attribute Profile (GATT)
+# +---------------------+--------+----------------------------------+
+# | Name                | Value  | Purpose                          |
+# +---------------------+--------+----------------------------------+
+# | Service Declaration | 0x2800 | what kind of service (see table) |
+# +---------------------+--------+----------------------------------+
+# | | 0x2800 |
+# +---------------------+--------+----------------------------------+
+
+
+print("Scanning for BLE devices... (indefinately)")
+
+service = DiscoveryService() # DiscoveryService("hci0")
+
+timeout = 5
+devices = []
+while len(devices) == 0:
+  devices = service.discover(timeout)
+  print(f"found {len(devices)} devices ({timeout} seconds)")
 
 for address, name in devices.items():
   print(f"name: {name}, address: {address}")
@@ -21,8 +36,12 @@ name = req.read_by_uuid("00002a00-0000-1000-8000-00805f9b34fb")[0]
 
 print(f"Services:")
 for serv in req.discover_primary():
-	print(f"uuid: {serv['uuid']}, start: {serv['start']}, end: {serv['end']}")
-for char in req.discover_characteristics():
-	print(f"uuid: {char['uuid']}, handle: {char['handle']}, value_handle: {char['value_handle']}, properties: {char['properties']}")
+  print(f"uuid: {serv['uuid']}, start: {serv['start']}, end: {serv['end']}")
+  # for i in req.read_by_uuid(serv['uuid']):
+    # print(f"--\n{i}")
 
+print(f"Characteristics:")
+for char in req.discover_characteristics():
+  print(f"uuid: {char['uuid']}, handle: {char['handle']}, value_handle: {char['value_handle']}, properties: {char['properties']}")
+# req.write_by_handle(0x10, bytes([14, 4, 56]))
 
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
+
+## references
+
+- https://www.bluetooth.com/specifications/le-audio/
+- https://people.csail.mit.edu/albert/bluez-intro/c212.html
+- https://github.com/labapart/gattlib
+- https://devzone.nordicsemi.com/nordic/short-range-guides/b/bluetooth-low-energy/posts/ble-characteristics-a-beginners-tutorial
+
 ## oof
 
 - https://stackoverflow.com/questions/54380867/pybluez-cant-find-devices-but-bluetoothctl-can
