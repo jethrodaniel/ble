cd78ab4e438566ed142d969a639517322f586777
Mark Delk <jethrodaniel@gmail.com>
2020-10-22 01:46:25 -0500

    start adding uuid db

--- blutooth.py
+++ blutooth.py
@@ -22,11 +22,17 @@ class UuidDatabase:
   def __init__(self):
     with open('third_party/bluetooth-numbers-database/v1/characteristic_uuids.json') as f:
       self.data = json.load(f)
+  def sanitize_uuid(self, uuid_value):
+    """Convert `00001800-0000-1000-8000-00805f9b34fb` to `1800`"""
+    return uuid_value[4:8]
   def uuid(self, uuid_value):
-    return filter(lambda row: row['uuid'] == uuid_value, self.data)[0]
-
-db = UuidDatabase()
-# print(db.data)
+    # res = list(filter(lambda row: row['uuid'] == self.sanitize_uuid(uuid_value), self.data))
+    res = list(filter(lambda row: row['uuid'] == uuid_value[4:8], self.data))
+    import pdb; pdb.set_trace()
+    if len(res) > 0:
+      return res[0]
+    else:
+      return ''
 
 print("Scanning for BLE devices... (indefinately)")
 
@@ -48,15 +54,14 @@ print("\n=== Devices ===")
 for address, name in devices.items():
   print(f"name: {name}, address: {address}")
 
+# required! `devices` likely points to the same GattLib C-object, which is
+# causing some mayham, leading to segmentation faults
+addresses = devices.copy()
 
 db = {
   'name': '00002a24-0000-1000-8000-00805f9b34fb',
   'bluetooth chip': '00002a29-0000-1000-8000-00805f9b34fb'
 }
-
-# required! `devices` likely points to the same GattLib C-object, which is
-# causing some mayham, leading to segmentation faults
-addresses = devices.copy()
 
 for address, name in addresses.items():
   req = ble.GATTRequester(address)
@@ -64,10 +69,10 @@ for address, name in addresses.items():
   print(f"\n==> [device]: {address} ({name}) ===")
   print(f"\nInfo we actually understand:")
   for name, uuid in db.items():
-    # import pdb; pdb.set_trace()
     print(f"{name}: {req.read_by_uuid(uuid)}")
     print(f"service type: {req.read_by_handle(0x001)}") # Generic Attribute
     print(f"service type: {req.read_by_handle(0x005)}") # Generic Access
+
 
   # Service values: https://www.bluetooth.com/specifications/assigned-numbers/
   #
@@ -75,17 +80,17 @@ for address, name in addresses.items():
   print(f"\nServices:")
   for serv in req.discover_primary():
     print(f"uuid: {serv['uuid']}, start: {serv['start']}, end: {serv['end']}")
-    import pdb; pdb.set_trace()
-    name = db.uuid(serv['uuid'])
-    print(f"name: {name}, uuid: {serv['uuid']}, start: {serv['start']}, end: {serv['end']}")
+    # import pdb; pdb.set_trace()
+    # name = UuidDatabase().uuid(serv['uuid'])
+    # print(f"name: {name}, uuid: {serv['uuid']}, start: {serv['start']}, end: {serv['end']}")
 
   # Characteristic values: https://www.bluetooth.com/specifications/assigned-numbers/
   #
   # | GATT Characteristic and Object Type | 0x2A00 | Device Name
   print(f"\nCharacteristics:")
   for char in req.discover_characteristics():
-    val = req.read_by_uuid(char['uuid'])
-    print(f"uuid: {char['uuid']}, handle: {char['handle']}, value_handle: {char['value_handle']}, properties: {char['properties']}, value: {val}")
+    name = UuidDatabase().uuid(serv['uuid'])
+    name = 'tmp'
+    print(f"name: {name}, uuid: {char['uuid']}, handle: {char['handle']}, value_handle: {char['value_handle']}, properties: {char['properties']}, value: {val}")
   req.disconnect()
-  # req.write_by_handle(0x10, bytes([14, 4, 56]))
 
