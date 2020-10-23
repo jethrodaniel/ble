65d710ef71d80a0e3f22eba6d5c829c20d5fe580
Mark Delk <jethrodaniel@gmail.com>
2020-10-23 02:43:01 -0500

    split out uuid_database

---
+++ blutooth/uuid_database.py
@@ -0,0 +1,15 @@
+import json
+
+class UuidDatabase:
+  def __init__(self):
+    with open('third_party/bluetooth-numbers-database/v1/characteristic_uuids.json') as f:
+      self.data = json.load(f)
+
+  def sanitize_uuid(self, uuid_value):
+    """Convert `00001800-0000-1000-8000-00805f9b34fb` to `1800`"""
+    return uuid_value[4:8]
+
+  def uuid(self, uuid_value):
+    res = list(filter(lambda row: row['uuid'] == str.upper(uuid_value[4:8]), self.data))
+    if len(res) > 0: return res[0]
+    else:            return ''
